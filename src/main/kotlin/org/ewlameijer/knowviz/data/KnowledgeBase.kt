d8dbbab55bfea938593aa0b06715b06d1565ede3
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2022-02-16 16:47:45 +0100

    Can now nicely display (and hence save!) data

--- src/main/kotlin/org/ewlameijer/knowviz/data/KnowledgeBase.kt
+++ src/main/kotlin/org/ewlameijer/knowviz/data/KnowledgeBase.kt
@@ -1,6 +1,20 @@
 package org.ewlameijer.knowviz.data
 
 class KnowledgeBase(private val concepts: Set<Concept>, private val relationships: Set<Relationship>) {
-    override fun toString() =
-        "Concepts:\n  ${concepts.joinToString("\n  ")}\nRelationships:\n  ${relationships.joinToString("\n  ")}"
+
+    override fun toString() = buildString {
+        val originToRelationships = relationships.groupBy { it.origin }
+        originToRelationships.forEach { (concept, relationships) ->
+            appendLine(concept)
+            relationships.forEach { relationship ->
+                appendLine("  ${relationship.relationshipType.text}")
+                if (relationship is OneToOneRelationship)
+                    appendLine("    ${relationship.destination}")
+                else (relationship as OneToManyRelationship).destinations.forEach { destination ->
+                    appendLine("    $destination")
+                }
+            }
+            appendLine()
+        }
+    }
 }
--- src/main/kotlin/org/ewlameijer/knowviz/data/Relationship.kt
+++ src/main/kotlin/org/ewlameijer/knowviz/data/Relationship.kt
@@ -1,28 +1,34 @@
 package org.ewlameijer.knowviz.data
 
 enum class RelationshipArity { OneToOne, OneToMany }
-enum class RelationshipType { IsA, CanHave }
-data class RelationshipData(val arity: RelationshipArity, val type: RelationshipType)
-
-val relationships = mapOf(
-    "is a(n)" to RelationshipData(RelationshipArity.OneToOne, RelationshipType.IsA),
-    "can have" to RelationshipData(RelationshipArity.OneToMany, RelationshipType.CanHave)
-)
+enum class RelationshipType(val text: String, val arity: RelationshipArity) {
+    IsA(
+        "is a(n)",
+        RelationshipArity.OneToOne
+    ),
+    CanHave(
+        "can have",
+        RelationshipArity.OneToMany
+    )
+}
 
 sealed class Relationship(val origin: Concept, val relationshipType: RelationshipType) {
     companion object {
         fun ofWithIsValid(source: Concept?, s: String?, destinations: Set<Concept>): Boolean =
-            if (source == null) false else
-                when (relationships[s]?.arity) {
+            if (source == null) false
+            else {
+                val selectedRelationship = RelationshipType.values().firstOrNull { it.text == s }
+                when (selectedRelationship?.arity) {
                     null -> false
                     RelationshipArity.OneToOne -> destinations.size == 1
                     RelationshipArity.OneToMany -> destinations.isNotEmpty()
                 }
+            }
 
         fun of(source: Concept?, s: String?, destinations: Set<Concept>): Relationship {
             require(ofWithIsValid(source, s, destinations))
-            val (relationshipArity, relationshipType) = relationships[s]!!
-            return if (relationshipArity == RelationshipArity.OneToOne) OneToOneRelationship(
+            val relationshipType = RelationshipType.values().first{ it.text == s }
+            return if (relationshipType.arity == RelationshipArity.OneToOne) OneToOneRelationship(
                 source!!,
                 relationshipType,
                 destinations.toList().first()
@@ -35,12 +41,12 @@ sealed class Relationship(val origin: Concept, val relationshipType: Relationshi
     }
 }
 
-class OneToOneRelationship(origin: Concept, relationshipType: RelationshipType, private val destination: Concept) :
+class OneToOneRelationship(origin: Concept, relationshipType: RelationshipType, val destination: Concept) :
     Relationship(origin, relationshipType) {
     override fun toString() = "$origin $relationshipType $destination"
 }
 
-class OneToManyRelationship(origin: Concept, relationshipType: RelationshipType, private val destinations: Set<Concept>) :
+class OneToManyRelationship(origin: Concept, relationshipType: RelationshipType, val destinations: Set<Concept>) :
     Relationship(origin, relationshipType) {
     override fun toString() = destinations.joinToString("\n") { "$origin $relationshipType $it" }
 }
