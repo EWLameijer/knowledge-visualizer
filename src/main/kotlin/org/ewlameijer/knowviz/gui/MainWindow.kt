d5c5a6069c5de4e2804d49705471f029699f0ef0
EWLameijer <eric_wubbo@hotmail.com>
2022-07-25 21:45:43 +0200

    Now buttons at random position. Next phase: make them repel each other

--- src/main/kotlin/org/ewlameijer/knowviz/gui/MainWindow.kt
+++ src/main/kotlin/org/ewlameijer/knowviz/gui/MainWindow.kt
@@ -2,21 +2,53 @@ package org.ewlameijer.knowviz.gui
 
 import org.ewlameijer.knowviz.data.KnowledgeBase
 import java.awt.Color
-import java.awt.FlowLayout
+import java.awt.Font
+import java.awt.FontMetrics
 import javax.swing.JButton
 import javax.swing.JFrame
+import kotlin.random.Random
+
+const val windowWidth = 1000
+const val windowHeight = 700
+const val buttonHeight = 30
+const val horizontalTextMargin = 20
 
 class MainWindow(knowledgeBase: KnowledgeBase) : JFrame() {
     init {
-        setSize(1000, 700)
+        setSize(windowWidth, windowHeight)
         defaultCloseOperation = EXIT_ON_CLOSE
         isVisible = true
         this.contentPane.background = Color.WHITE
-        layout = FlowLayout()
+        layout = null
         knowledgeBase.concepts().toList().forEach {
             println(it.text)
-            val button = JButton(it.text)
+            val button = MovableButtonComponent(it.text)
             add(button)
         }
+        repaint()
     }
+}
+
+class MovableButtonComponent(text: String) : JButton(text) {
+    init {
+        val textWidth = getTextWidth(this.font, text)
+
+        val thisButtonWidth = textWidth + 2 * horizontalTextMargin
+        this.setBounds(randomX(thisButtonWidth), randomY(), thisButtonWidth, buttonHeight)
+    }
+}
+
+val randomizer = Random.Default
+
+fun randomX(textWidth: Int): Int = randomizer.nextInt(windowWidth - textWidth - 10)
+
+fun randomY(): Int = randomizer.nextInt(windowHeight - buttonHeight)
+
+//https://stackoverflow.com/questions/69042679/is-there-a-function-to-find-the-amount-of-space-a-text-inside-a-button-will-take
+fun getTextWidth(font: Font?, text: String?): Int {
+    val metrics: FontMetrics = object : FontMetrics(font) {
+        private val serialVersionUID = 345265L
+    }
+    val bounds = metrics.getStringBounds(text, null)
+    return bounds.width.toInt()
 }
